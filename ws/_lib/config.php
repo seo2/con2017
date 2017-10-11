66a4a888a811b55b3fc6d61c965e1acc9da81acd
Seo2 <seo2@seo2.cl>
2017-10-11 17:33:51 -0300

    a

--- ws/_lib/config.php
+++ ws/_lib/config.php
@@ -2,5 +2,5 @@
 	define("DBHOST", "internal-db.s219687.gridserver.com");
 	define("DBUSER", "db219687");
 	define("DBPASS", "4#LyMm}-3tm");
-	define("DBNAME", "db219687_coq2017");
+	define("DBNAME", "db219687_con");
 ?>
